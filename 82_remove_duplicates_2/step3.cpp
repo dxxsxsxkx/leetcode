6ce7e139c09e5b6629cacf4530c63b6f2f669098
dxxsxsxkx <tosshantosshan2000@gmail.com>
2026-01-01 16:55:49 +0900

    step3

---
+++ 82_remove_duplicates_2/step3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy(-1000);
+        dummy.next = head;
+
+        ListNode* node_current = &dummy;
+        ListNode* runner = head;
+
+        while (runner != nullptr && runner->next != nullptr) {
+            if (runner->val == runner->next->val) {
+                int val_duplicate = runner->val;
+
+                while (runner->val == val_duplicate) {
+                    ListNode* node_duplicate = runner;
+                    runner = runner->next;
+                    delete node_duplicate;
+                }
+
+                node_current->next = runner;
+
+            } else {
+                node_current = runner;
+                runner = runner->next;
+            }
+        }
+
+        return dummy.next;
+    }
+};
