120723663066061e4a3a7b055e82339a6a2c3ae9
dxxsxsxkx <tosshantosshan2000@gmail.com>
2026-02-16 23:50:15 +0900

    276. Paint Fence

---
+++ 276_paint_fence/step1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    /**
+     * @param n: non-negative integer, n posts
+     * @param k: non-negative integer, k colors
+     * @return: an integer, the total number of ways
+     */
+    int numWays(int n, int k) {
+        // write your code here
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return k;
+        }
+        if (n == 2) {
+            return k * k;
+        }
+
+        int n_ways = 0;
+        int previous_n_ways = k * k;
+        int second_previous_n_ways = k;
+        for (int i = 2; i < n; i++) {
+            n_ways = (k - 1) * (previous_n_ways + second_previous_n_ways);
+            second_previous_n_ways = previous_n_ways;
+            previous_n_ways = n_ways;
+        }
+        return n_ways;
+    }
+};
